ebfc543f9ff0f46313502105154d21c96063a1f9
SSobol77 <ssobo77@gmail.com>
2026-06-12 17:53:59 +0200

    fix language error in string

--- app/page.tsx
+++ app/page.tsx
@@ -638,7 +638,7 @@ export default function Home() {
           <div className="pt-8 border-t border-slate-800 flex flex-col sm:flex-row justify-between items-center gap-4 text-sm">
             <p>© 2026 Cartesian School. All rights reserved.</p>
             <p className="flex items-center gap-2">
-              Powered on FreeBSD
+              Powered by FreeBSD
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src="/freebsd.png" alt="FreeBSD logo" className="h-4 w-auto" />
             </p>
